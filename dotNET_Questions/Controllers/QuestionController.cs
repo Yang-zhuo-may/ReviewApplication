805a53ad5c39684fb102c48876b8ec18f2e87620
Zhuomeiduo Yang <z.yang@interface3.be>
2024-11-21 14:01:48 +0100

    add the delete function

--- dotNET_Questions/Controllers/QuestionController.cs
+++ dotNET_Questions/Controllers/QuestionController.cs
@@ -64,9 +64,44 @@ namespace dotNET_Questions.Controllers
             {
                 _db.Questions.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "The question edited successfully!";
                 return RedirectToAction("Index");
             }
             return View(obj);
+        }
+
+        // GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var questionFromDb = _db.Questions.Find(id);
+
+            if (questionFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(questionFromDb);
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Questions.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Questions.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "The question deleted successfully!";
+            return RedirectToAction("Index");
         }
     }
 }
